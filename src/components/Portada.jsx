b1e96ee1b4a3c84e9ac86aae169cef9c5106d5c8
DaniSanchez0707 <danisanc927@gmail.com>
2024-07-09 14:00:09 -0600

    Mejora
    Mejora

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -10,7 +10,7 @@ function Portada() {
    <div className='grid'>
   <h3 className='float-left justify-center col-start-1 text-2xl'>Bienvenidos a InnovaBytes</h3>
    
-  <img  src="../../public/InnovaBytes-Logo.svg" className='float-left col-start-3 w-64 h-64' />
+
    </div>
 
 
