fc8509e2b8dd118679826706cf8ad5f78481d2e6
DaniSanchez0707 <danisanc927@gmail.com>
2024-07-09 14:18:22 -0600

    Mejora
    Mejora

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -12,7 +12,12 @@ function Portada() {
    
 
    </div>
-
+   <img
+        className="w-1/4 h-1/2 img-container"
+        src="https://static.vecteezy.com/system/resources/thumbnails/011/153/368/small_2x/3d-website-developer-working-on-laptop-illustration-png.png"
+        alt=""
+     
+      />
 
 </>
   )
--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function Servicios() {
   return (
-    <div>Servicios
+    <div style={{ textAlign: 'center' }}>
+      <h1>Servicios</h1>
       <br />
       <br />
-      <br />
-      <br />
-        <p class="text-left">
-        En InnovaBytes se ofrece el 
-        desarrollo de aplicaciones web innovadoras y personalizadas para
+     
+      <p className="text-left">
+        En InnovaBytes se ofrece el desarrollo de aplicaciones web innovadoras y personalizadas para
         satisfacer las necesidades de tu negocio.
       </p>
+   
+      <img
+        className="w-1/4 h-1/2 img-container"
+        src="https://static.vecteezy.com/system/resources/previews/011/153/359/original/3d-website-developer-working-on-laptop-illustration-png.png"
+        alt=""
+   
+      />
     </div>
-  )
+  );
 }
 
-export default Servicios
+export default Servicios;
