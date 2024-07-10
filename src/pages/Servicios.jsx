cbb982d990e5ed7a9b627416a9485626c3cb7c3b
DaniSanchez0707 <danisanc927@gmail.com>
2024-07-10 17:28:48 -0600

    Mejora
    Mejora

--- src/pages/Servicios.jsx
+++ src/pages/Servicios.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function Servicios() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: "center" }}>
       <h1>Servicios</h1>
       <br />
       <br />
-     
+
       <p className="text-left">
-        En InnovaBytes se ofrece el desarrollo de aplicaciones web innovadoras y personalizadas para
-        satisfacer las necesidades de tu negocio.
+        En InnovaBytes se ofrece el servicio como freelancers en el desarrollo
+        de aplicaciones web para pequeñas y medianas empresas (PYMES)🌐.
       </p>
-   
+
       <img
         className="w-1/4 h-1/2 img-container"
         src="https://static.vecteezy.com/system/resources/previews/011/153/359/original/3d-website-developer-working-on-laptop-illustration-png.png"
         alt=""
-   
       />
     </div>
   );
