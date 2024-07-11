9485e270ed534a9f8e938700533f50b0a2b40336
DaniSanchez0707 <danisanc927@gmail.com>
2024-07-10 18:08:24 -0600

    Mejora
    Mejora

--- src/pages/Contactos.jsx
+++ src/pages/Contactos.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 function Contactos() {
   // Reemplaza el número con el que deseas iniciar el chat en WhatsApp
-  const message='Hola, estoy interesado en tus servicios. ¿Podrías proporcionarme más información?'
-  const phonenumber = '+50433304828'
-    const phonenumber2 = '+50489858989'
+  const message='Hola, estoy interesado en tus servicios como desarrollador freelance. ¿Podrías proporcionarme más información?'
+  const phonenumber = '+50487692973'
   const whatsappLink = `https://wa.me/${phonenumber}?text=${encodeURIComponent(message)}`;
-  const whatsappLink2 = `https://wa.me/${phonenumber2}?text=${encodeURIComponent(message)}`;
+
   return (
     <div>Contactos
     
@@ -24,14 +23,10 @@ function Contactos() {
       <div className="flex flex-col gap-4">
       <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Contactar por WhatsApp Enlace 1
+          Contactar por WhatsApp
         </button>
       </a>
-      <a href={whatsappLink2} target="_blank" rel="noopener noreferrer">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Contactar por WhatsApp Enlace 2
-        </button>
-      </a>
+ 
     </div>
       <br />
      
