c8df3aca8f00d2e3b29741a8a8045c1f673e34f1
DaniSanchez0707 <danisanc927@gmail.com>
2024-07-11 18:29:23 -0600

    Mejora
    Mejora

--- src/components/Portada.jsx
+++ src/components/Portada.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
+import React from 'react';
+
 
 function Portada() {
   return (
-    <>
-<br/>
-<br/>
-<br/>
-<br/>
-   <div className='grid'>
-  <h3 className='float-left justify-center col-start-1 text-2xl'>Bienvenidos a InnovaBytes</h3>
-   
-
-   </div>
-   <img
-        className="w-1/4 h-1/2 img-container"
-        src="https://static.vecteezy.com/system/resources/thumbnails/011/153/368/small_2x/3d-website-developer-working-on-laptop-illustration-png.png"
-        alt=""
-     
-      />
-<br/>
-<p className="text-left">
-      Somos Freelancers que ofrecemos el servicio de desarrollo de aplicaciones web para pequeñas y medianas empresas (PYMES) o personas que buscan una solución tecnológica.🌐 
-      </p>
-
-</>
-  )
+ 
+      <div className="container">
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <div className='grid'>
+          <h3 className='float-left justify-center col-start-1 text-2xl'>Bienvenidos a InnovaBytes</h3>
+        </div>
+        <img
+          className="w-1/4 h-1/2 img-container"
+          src="https://static.vecteezy.com/system/resources/thumbnails/011/153/368/small_2x/3d-website-developer-working-on-laptop-illustration-png.png"
+          alt=""
+        />
+        <br/>
+        <p className="text-left">
+          Somos Freelancers que ofrecemos el servicio de desarrollo de aplicaciones web para pequeñas y medianas empresas (PYMES) o personas que buscan una solución tecnológica.🌐 
+        </p>
+        <br/>
+        <br />
+        <a href="https://porfolio-carlos-arturo.vercel.app/">Carlos Hernandez: porfolio-carlos-arturo.vercel.app/</a>
+        <br />
+        <a href="https://perfil-dani-sanchez.vercel.app/">Dani Sanchez: perfil-dani-sanchez.vercel.app</a>
+      </div>
+  );
 }
 
-export default Portada
+export default Portada;
